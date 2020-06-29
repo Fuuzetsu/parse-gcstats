fce42e0952a9d8f3583b7755e1c065911ed19e2d
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2020-06-29 17:13:08 +0900

    Incrementally-runnable parsers
    Allows to use something like conduit to run on streaming content.

--- src/Data/GCStats/Parse.hs
+++ src/Data/GCStats/Parse.hs
@@ -1,11 +1,16 @@
 module Data.GCStats.Parse
   ( gcStatsParser
+  , gcStatsIncr
+  , gcStatsIncrSkipEnd
+  , gcStatsIncrFirstEntry
   ) where
 
-import Data.String
+import Control.Applicative (optional, (<|>))
+import Control.Monad (void)
 import qualified Data.Attoparsec.Text as P
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HashMap
+import Data.String
 import Data.Text
 import qualified Data.Text as T
 import GHC.Stats
@@ -14,14 +19,52 @@ import GHC.Stats
 -- 'Double' though many are actually integers.
 gcStatsParser :: P.Parser (HashMap Text Double)
 gcStatsParser = do
-  -- Skip first line which contains the command-line executed.
-  P.takeTill P.isEndOfLine <* P.endOfLine
-  _ <- P.skipSpace *> P.char '['
-  entries <- entry `P.sepBy` (P.skipSpace <* P.char ',')
+  m'firstEntry <- gcStatsIncrFirstEntry
+  entries <- case m'firstEntry of
+    -- No point trying to parse more if we can't even get first entry.
+    Nothing -> pure mempty
+    Just firstEntry -> (:) firstEntry <$> P.many' nextEntry
   -- We've parsed all the entries there were, for completion and to
   -- ensure nothing went wrong, expect the terminating bracket.
-  _ <- P.skipSpace *> P.char ']'
+  gcStatsIncrSkipEnd
   pure $! HashMap.fromList entries
+
+-- | Skip start of file which contains command line use and yield
+-- first entry, if there is one. You should run this before
+-- 'gcStatsIncr'.
+gcStatsIncrFirstEntry :: P.Parser (Maybe (Text, Double))
+gcStatsIncrFirstEntry = do
+  P.skipWhile (not . P.isEndOfLine)
+  P.skipSpace
+  void $ P.char '['
+  optional entry
+
+-- | Check if we have a closing brace, indicating end of entries.
+--
+-- Does not check for EOF.
+gcStatsIncrSkipEnd :: P.Parser ()
+gcStatsIncrSkipEnd = P.skipSpace <* P.char ']'
+
+-- | Parser that returns entries as they come in, effectively
+-- tokenising the input. In interest of streaming, does no input
+-- validation on whether the sequence of tokens it produces actually
+-- makes sense.
+--
+-- Whatever way you're parsing, you ought to first do one run of
+-- 'gcStatsIncrFirstEntry' after which you can invoke 'gcStatsIncr'
+-- until it no longer matches. This function returns @Nothing@ if end
+-- of entries is seen.
+gcStatsIncr :: P.Parser (Maybe (Text, Double))
+gcStatsIncr = (Just <$> nextEntry) <|> (Nothing <$ gcStatsIncrSkipEnd)
+
+entry :: P.Parser (Text, Double)
+entry = do
+  _ <- P.char '('
+  k <- key
+  _ <- fromString ", "
+  v <- val
+  _ <- P.char ')'
+  pure (k, v)
   where
     key :: P.Parser Text
     key = P.char '"' *> P.takeTill (== '"') <* P.char '"'
@@ -29,11 +72,10 @@ gcStatsParser = do
     val :: P.Parser Double
     val = P.char '"' *> P.double <* P.char '"'
 
-    entry :: P.Parser (Text, Double)
-    entry = do
-      _ <- P.char '('
-      k <- key
-      _ <- fromString ", "
-      v <- val
-      _ <- P.char ')'
-      pure (k, v)
+-- | Some non-first entry.
+nextEntry :: P.Parser (Text, Double)
+nextEntry = do
+  -- Non-first entry, has to have separator in front.
+  P.skipSpace *> P.char ',' *> P.skipSpace
+  -- We should be at an entry now, output it.
+  entry
--- test/ParseTest.hs
+++ test/ParseTest.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
+import Control.Monad (forM_)
 import Data.Attoparsec.Text (parseOnly)
+import Data.Conduit
+import qualified Data.Conduit.Attoparsec as C
+import qualified Data.Conduit.Combinators as C
 import Data.GCStats.Parse
+import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import System.FilePath (takeBaseName, (<.>), replaceExtension)
 import Test.Tasty (defaultMain, TestTree, testGroup)
@@ -13,8 +19,10 @@ main = goldenTests >>= defaultMain
 goldenTests :: IO TestTree
 goldenTests = do
   gcStatsFiles <- findByExtension [".gcstats"] "."
-  pure $ testGroup "gcStatsParser golden tests"
-    (parseTest <$> gcStatsFiles)
+  pure $ testGroup "gcStatsParser golden tests" $ mconcat
+    [ parseTest <$> gcStatsFiles
+    , parseTestIncr <$> gcStatsFiles
+    ]
   where
     parseTest :: FilePath -> TestTree
     parseTest gcStats =
@@ -24,3 +32,36 @@ goldenTests = do
          goldenFile
          outFile
          (parseOnly gcStatsParser <$> T.readFile gcStats >>= writeFile outFile . show)
+
+    -- Test incremental parsing with conduit
+    parseTestIncr :: FilePath -> TestTree
+    parseTestIncr gcStats =
+      let goldenFile = gcStats <.> "incremental.golden"
+          outFile = replaceExtension goldenFile "out"
+
+          incrParser = do
+            let showT = T.pack . show
+                render (pos, Nothing) = "END - " <> showT pos
+                render (pos, Just (k, v)) =
+                    k <> ": " <> T.pack (show v) <> " - " <> showT pos
+                consumeEntries = await >>= \m'r -> forM_ m'r $ \r -> case r of
+                  (_, Nothing) -> yield $ render r
+                  (_, Just{}) -> do
+                    yield $ render r
+                    consumeEntries
+            -- Get first entry, put it back on stream for reading.
+            m'first <- C.conduitParser gcStatsIncrFirstEntry .| C.head
+            case m'first of
+              Nothing -> pure ()
+              Just firstEntry -> yield $ render firstEntry
+            C.conduitParser gcStatsIncr .| consumeEntries
+
+          act = runConduitRes $ C.sourceFile gcStats
+            .| C.decodeUtf8
+            .| incrParser
+            .| C.unlines
+            .| C.encodeUtf8
+            .| C.sinkFile outFile
+
+      in goldenVsFile (takeBaseName gcStats <.> "incremental")
+         goldenFile outFile act
